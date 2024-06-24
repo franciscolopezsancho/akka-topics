1d2493b497598ac90ea62a413e39d99098adfacf
Vasile Gorcinschi <vasigorc@users.noreply.github.com>
2024-06-24 18:10:16 -0400

    Update betting-house/common-deployment/init_psql.sh
    Co-authored-by: Francisco Lopez-Sancho <francisco.lopezsancho@lightbend.com>

--- betting-house/common-deployment/init_psql.sh
+++ betting-house/common-deployment/init_psql.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+apt-get update && \
+apt-get install -y gosu && \
+rm -rf /var/lib/apt/lists/*
+
 # Copy .sql files to PostgreSQL data directory
 cp /init-scripts/*.sql /docker-entrypoint-initdb.d/
 
